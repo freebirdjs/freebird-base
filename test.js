e1636638010f43ce5fd7296d6845437165b97a46
Simen Li <simenkid@gmail.com>
2016-04-02 18:12:56 +0800

    netcore, device, gadget classes

--- test.js
+++ test.js
@@ -1,2 +1,21 @@
-var now = Math.floor(Date.now()/1000);
-console.log(now);
+var fun1 = function (x1, args) {
+    console.log(x1);
+    console.log(args);
+
+    var len = args.length;
+    var cb = args[len - 1];
+    if (typeof cb === 'function')
+        cb();
+};
+
+var fun2 = function (p1, p2, p3, cb) {
+    console.log('arguments');
+    console.log(arguments);
+    return fun1('hello', arguments);
+};
+
+var fun3 = function () 
+
+fun2('p1', 'p2', 'p3', function () {
+    console.log('done');
+});
