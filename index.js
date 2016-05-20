18bcbde101719b3b4693550634be78e87befd7f6
Simen Li <simenkid@gmail.com>
2016-05-20 10:32:23 +0800

    not export custom error at this moment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     Netcore: require('./lib/netcore.js'),
     Device: require('./lib/device.js'),
     Gadget: require('./lib/gadget.js'),
-    Errors: require('./lib/errors.js')
+//    Errors: require('./lib/errors.js')    //[TODO]
 };
