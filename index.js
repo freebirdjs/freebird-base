7dd63425b9b5593ef78b704ede3932e3a6c1a398
Simen Li <simenkid@gmail.com>
2016-06-13 16:37:54 +0800

    add createNetcore() API

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+var Netcore = require('./lib/netcore.js'),
+    Device = require('./lib/device.js'),
+    Gadget = require('./lib/gadget.js');
+
 module.exports = {
-    Netcore: require('./lib/netcore.js'),
-    Device: require('./lib/device.js'),
-    Gadget: require('./lib/gadget.js'),
+    Netcore: Netcore,
+    Device: Device,
+    Gadget: Gadget,
+    createNetcore: function (name, controller, protocol, opt) {
+        return new Netcore(name, controller, protocol, opt);
+    }
 //    Errors: require('./lib/errors.js')    //[TODO]
 };
